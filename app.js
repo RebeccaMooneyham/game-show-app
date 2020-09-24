bbc54a9a80d88766beaef58601be42e0a30c8466
Rebecca <mooneyhamrebecca@gmail.com>
2020-09-24 16:55:10 -0600

    2nd Commit -create function getRandomPhraseAsArray

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startButton = document.querySelector('.btn_reset');
-
 let missed = 0;
 const phrases = ['Eat Drink And Be Merry', 'Buy One Get One Free', 'Can You Keep A Secret', 'Go Jump In A Lake', 'Look On The Bright Side'];
 
-// return a random phrase from an array
-// const getRandomPhraseAsArray = arr => {
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+  const overlay = document.getElementById('overlay');
+  overlay.style.display = 'none'
+});
 
-// }
+// return a random phrase from an array
+function getRandomPhraseAsArray(arr) {
+  //create a variable to store a random number based on the length of the array
+  const randomNumber = Math.floor(Math.random() * phrases.length + 1);
+
+  //use the variable to select an index inside of the array and split into individual words
+  const chosenPhrase = phrases[randomNumber].split(" ");
+
+  // return the array element at that index
+  return chosenPhrase;
+}
+
+getRandomPhraseAsArray(phrases);
 
 // add the letters of a string to the display
 // const addPhraseToDisplay = arr => {
@@ -25,11 +39,6 @@ const phrases = ['Eat Drink And Be Merry', 'Buy One Get One Free', 'Can You Keep
 
 // }
 
-// listen for the start game button to be pressed
-startButton.addEventListener('click', () => {
-  const overlay = document.getElementById('overlay');
-  overlay.style.display = 'none'
-});
 
 // listen for the onscreen keyboard to be clicked
 // qwerty.addEventListener('click' e => {
